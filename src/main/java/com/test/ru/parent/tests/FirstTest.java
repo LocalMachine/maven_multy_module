f8596464b992609c752f95f65cd88d20a797862a
Manukyan Rachik <rmanukyan@softailor.ru>
2023-08-03 15:17:31 +0300

    create empty project

---
+++ src/main/java/com/test/ru/parent/tests/FirstTest.java
@@ -0,0 +1,11 @@
+package com.test.ru.parent.tests;
+
+import com.test.ru.ui_core.utils.LogHelper;
+
+import static com.test.ru.ui_core.utils.LogHelper.printHello;
+
+public class FirstTest {
+    public void useChildModuleMethod() {
+        printHello();
+    }
+}
